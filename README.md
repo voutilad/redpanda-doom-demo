96587c88989569bda212ab6ccd7b3e92463f3688
Dave Voutila <voutilad@gmail.com>
2024-01-17 12:06:16 -0500

    update README.md, cleanup run.sh

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-beep boop
+# Doom --> Websockets + MQTT --> Zilla --> Redpanda
+
+```
+$ git clone https://github.com/voutilad/redpanda-doom-demo.git
+$ git submodule update --init --recursive
+```
+
+## Chocolate Doom Telemetry
+
+Firstly, you'll need to build _Chocolate Doom_ from source using the branch in
+the included git [submodule](./chocolate-doom/).
+
+You'll need to use `chocolate-doom-setup` to configure the telemetry mode and
+point it towards Zilla. You should use the following settings for now:
+
+```
+telemetry_enabled             1
+telemetry_mode                5
+telemetry_ws_host             "debian-gnu-linux"
+telemetry_ws_port             7114
+telemetry_ws_path             "/redpanda"
+telemetry_ws_tls_enabled      0
+```
+
+## Aklivity Zilla
+
+Currently, this demo requires building Zilla from source as we need commits
+made after the `0.9.63` release that fix issues with MQTT over Websockets.
+
+## Redpanda
+
+I've been testing with _Redpanda Serverless_. Your mileage may vary with other
+versions.
--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ ZILLA_PORT_PROMETHEUS="${ZILLA_PORT_PROMETHEUS:-7190}"
 # Redpanda configs. 
 # Note: we split the broker seed host:port into parts. We also lowercase the
 #       sasl mechanism because that's what Zilla wants :(
+#  XXX: for now, we assume we're using SASL_SSL
 REDPANDA_BROKERS="${REDPANDA_BROKERS:-localhost:9092}"
 REDPANDA_SASL_MECHANISM="$(echo ${RP_SASL_MECHANISM:-SCRAM-SHA-256} | tr '[:upper:]' '[:lower:]')"
 REDPANDA_SASL_USERNAME="${REDPANDA_SASL_USERNAME:-zilla}"
@@ -19,9 +20,15 @@ REDPANDA_SASL_PASSWORD="${REDPANDA_SASL_PASSWORD}"
 REDPANDA_HOST="$(echo ${REDPANDA_BROKERS} | awk -F ':' '{ print $1 }')"
 REDPANDA_PORT="$(echo ${REDPANDA_BROKERS} | awk -F ':' '{ print $2 }')"
 
+# Java stuff. We default to debug mode on for logging.
+# XXX: for remote debugger, use:
+#      -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:8000
+JAVA_OPTIONS="${JAVA_OPTIONS:--Dzilla.binding.kafka.debug=true}"
+
 # Make rocket go now.
-docker run -e "JAVA_OPTIONS=-Dzilla.binding.kafka.debug=true" \
+docker run \
     -p 8000:8000 -p 7190:7190 -p 7114:7114 -p 7183:7183 \
+    --env "JAVA_OPTIONS=${JAVA_OPTIONS}" \
     --env "ZILLA_VERSION=${ZILLA_VERSION}" \
     --env "ZILLA_LISTEN_ON=${ZILLA_LISTEN_ON}" \
     --env "ZILLA_HOST=${ZILLA_HOST}" \
@@ -33,6 +40,6 @@ docker run -e "JAVA_OPTIONS=-Dzilla.binding.kafka.debug=true" \
     --env "REDPANDA_SASL_PASSWORD=${REDPANDA_SASL_PASSWORD}" \
     --env "REDPANDA_HOST=${REDPANDA_HOST}" \
     --env "REDPANDA_PORT=${REDPANDA_PORT}" \
-    -v ./zilla.yaml:/etc/zilla/zilla.yaml \
+    -v ./zilla/zilla.yaml:/etc/zilla/zilla.yaml \
     "ghcr.io/aklivity/zilla:${ZILLA_VERSION}" \
     start -v
